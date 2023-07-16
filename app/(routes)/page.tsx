ebdc3b7f0c00180078d7c92de82fe2ce0897018f
Eliyahu Kriel <elik56a@gmail.com>
2023-07-16 21:47:18 +0300

    init

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,12 +8,12 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("a161867a-c0cd-4e81-b0da-e2361b041c16");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard 
+        <Billboard
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
